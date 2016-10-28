d6f57fb9434a7e26888476e28480824b523602f2
Mike Dingjan <m.dingjan@lukkien.com>
2016-10-28 15:28:20 +0200

    Add default options

--- wagtailformblocks/utils/conf.py
+++ wagtailformblocks/utils/conf.py
@@ -6,6 +6,7 @@ from django.utils.translation import ugettext_lazy as _
 DEFAULTS = {
     'SUCCESS_MSG': _('Thank you, the form has been submitted.'),
     'ERROR_MSG': _('There was an error processing the form'),
+    'RECAPTCHA_ATTRS': {}
 }
 
 
