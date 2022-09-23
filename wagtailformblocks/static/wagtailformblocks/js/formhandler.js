64b36de168592522fe21500e67b47c307cf11ca2
Maarten van der Lei <m.vanderlei@lukkien.com>
2022-09-23 10:21:15 +0200

    update JS

--- wagtailformblocks/static/wagtailformblocks/js/formhandler.js
+++ wagtailformblocks/static/wagtailformblocks/js/formhandler.js
@@ -1,51 +1,53 @@
 function getCookie(name) {
-    var cookieValue = null;
-    if (document.cookie && document.cookie !== '') {
-        var cookies = document.cookie.split(';');
-        for (var i = 0; i < cookies.length; i++) {
-            var cookie = jQuery.trim(cookies[i]);
+    let cookieValue = null;
+    if (document.cookie && document.cookie !== "") {
+        const cookies = document.cookie.split(";");
+        for (let i = 0; i < cookies.length; i++) {
+            const cookie = jQuery.trim(cookies[i]);
             // Does this cookie string begin with the name we want?
-            if (cookie.substring(0, name.length + 1) === (name + '=')) {
-                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+            if (cookie.substring(0, name.length + 1) === name + "=") {
+                cookieValue = decodeURIComponent(
+                    cookie.substring(name.length + 1)
+                );
                 break;
             }
         }
     }
     return cookieValue;
 }
-var csrftoken = getCookie('csrftoken');
+const csrftoken = getCookie("csrftoken");
 function csrfSafeMethod(method) {
     // these HTTP methods do not require CSRF protection
-    return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
+    return /^(GET|HEAD|OPTIONS|TRACE)$/.test(method);
 }
 $.ajaxSetup({
-    beforeSend: function(xhr, settings) {
+    beforeSend: function (xhr, settings) {
         if (!csrfSafeMethod(settings.type) && !this.crossDomain) {
             xhr.setRequestHeader("X-CSRFToken", csrftoken);
         }
-    }
+    },
 });
 
 function showSuccess(form, message) {
-    form.replaceWith('<p class=\'success\'>' + message + '</p>');
+    form.replaceWith("<p class='success'>" + message + "</p>");
 }
 
 function showError(form, message) {
-    form.children('p.error').remove();
-    form.prepend('<p class=\'error\'>' + message + '</p>');
+    form.children("p.error").remove();
+    form.prepend("<p class='error'>" + message + "</p>");
 }
 
-$(document).on('submit', "form[id^='wagtailformblock_']", function(e) {
+$(document).on("submit", "form[id^='wagtailformblock_']", (e) => {
     // Prevent the form from submitting on it's own
     e.preventDefault();
 
-    var $form = $(e.target);
-    var action = $form.attr('action');
-    var data = $form.serializeArray();
+    const $form = $(e.target);
+    const action = $form.attr("action");
+    const data = $form.serializeArray();
 
-    $.post(action, data, function(data) {
+    $.post(action, data, function (data) {
         showSuccess($form, data.message);
-    }).fail(function(err) {
+    }).fail(function (err) {
         showError($form, err.responseJSON.message);
     });
 });
