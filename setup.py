fcdfab59d998d827847489237eb7927797a49581
Diederik van der Boor <d.vanderboor@lukkien.com>
2023-02-14 09:48:53 +0100

    Bump to 0.9.0

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ docs_require = [
 
 setup(
     name='wagtailformblocks',
-    version='0.8.0',
+    version='0.9.0',
     description="A Wagtail module that provides content blocks to display and process user defined forms",
     long_description=readme + '\n\n' + changelog,
     author="Tim Leguijt",
